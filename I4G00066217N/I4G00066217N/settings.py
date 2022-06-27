bb369533ead1ce7f0f768c362291e820ff941cee
Thelordgreat <greatokafor15@gmail.com>
2022-06-27 14:28:53 +0100

    'updates'

--- I4G00066217N/I4G00066217N/settings.py
+++ I4G00066217N/I4G00066217N/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^7@nbbss_hearqvawm69c9v47g&y2)=8a=uusbp7bnj_zq^g)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("TRUE")
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.app.BlogConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
